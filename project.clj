c7833570b861ca8366f13d5c058a6abf48e5a680
Vedang Manerikar <ved.manerikar@gmail.com>
2013-09-07 22:15:05 +0530

    Add LGPL implementation of MurmurHash3.
    Author: Patrick McFarland <diablod3@gmail.com>
    Refer: http://adterrasperaspera.com/images/MurmurHash3.java

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject me.vedang/bloomclj "0.1.0-SNAPSHOT"
   :description "A Bloom Filter implementation in Clojure."
   :url "http://github.com/vedang/bloomclj"
+  :java-source-paths ["src/java"]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
