8b065a451eb30a70bf9b7e9a2ccc5bf5c3725b61
Vedang Manerikar <ved.manerikar@gmail.com>
2013-09-08 12:23:14 +0530

    Add implementation of TransientBloomFilter
    Uses a java.util.BitSet bitarray to store the bitvals indicating
    membership in the Bloom Filter.

--- src/bloomclj/core.clj
+++ src/bloomclj/core.clj
@@ -1,4 +1,36 @@
-(ns bloomclj.core)
+(ns bloomclj.core
+  (:require [bloomclj.protocols :refer [to-byte-array IFilter]])
+  (:import (java.util BitSet)
+           (bloomjava.util.hash MurmurHash3)))
+
+
+(defn- get-hash-buckets
+  [e k m]
+  (let [b (to-byte-array e)
+        [h1 h2] (MurmurHash3/MurmurHash3_x64_128 b 0)]
+    (map #(Math/abs (long (mod (+' h1 (*' % h2) ) m))) (range 1 (inc k)))))
+
+
+(deftype TransientBloomFilter
+    [^:unsynchronized-mutable bitarray n fpp m k]
+  ;; "n - the maximum number of elements that will be inserted into the bloom
+  ;;      filter. This defines the capacity of the bloom filter. Inserting more
+  ;;      elements than n causes `fpp' to increase.
+  ;;  fpp - The desired false positive probability.
+  ;;  m - the total number of bits in the bit array. Calculated for optimal val
+  ;;      using n and fpp
+  ;;  k - The optimal number of hash functions that should be used. Calculated
+  ;;      using n and fpp"
+  IFilter
+  (add [this elem]
+    (doseq [b (get-hash-buckets elem (.k this) (.m this))]
+      (.set (.bitarray this) b)))
+
+  (maybe-contains? [this elem]
+    (every? identity (map #(.get (.bitarray this) %)
+                          (get-hash-buckets elem (.k this) (.m this))))))
+
+
 (defonce LN2 (Math/log 2))
 
 
@@ -21,3 +53,11 @@
               inserting the element in the Bloom Filter."
   [m n]
   (Math/ceil (/ (* m LN2) n)))
+
+
+(defn transient-bloom-filter
+  "Construct and return a TransientBloomFilter."
+  [n fpp]
+  (let [m (get-optimal-m n fpp)
+        k (get-optimal-k m n)]
+    (TransientBloomFilter. (BitSet. m) n fpp m k)))
---
+++ src/bloomclj/protocols.clj
@@ -0,0 +1,62 @@
+(ns bloomclj.protocols)
+
+
+;;; Protocol IByteArray - convert input to byte array.
+;;; Contributed by: Kiran Kulkarni <kiran.1267@gmail.com>
+;;; ===================================================
+(defprotocol IByteArray
+  (to-byte-array [this]))
+
+
+(extend-protocol IByteArray
+  nil
+  (to-byte-array [x] (byte-array 0))
+
+  java.lang.String
+  (to-byte-array [s] (.getBytes s "utf-8"))
+
+  java.lang.Short
+  (to-byte-array [short-int] (.. (java.nio.ByteBuffer/allocate 2)
+                                 (putShort short-int)
+                                 array))
+
+  java.lang.Integer
+  (to-byte-array [integer] (.. (java.nio.ByteBuffer/allocate 4)
+                               (putShort integer)
+                               array))
+
+  java.lang.Long
+  (to-byte-array [long-int] (.. (java.nio.ByteBuffer/allocate 8)
+                                (putShort long-int)
+                                array))
+
+  java.nio.ByteBuffer
+  (to-byte-array [byte-buffer] (.array byte-buffer))
+
+  clojure.lang.Keyword
+  (to-byte-array [k] (.getBytes (name k) "utf-8"))
+
+  clojure.lang.IPersistentList
+  (to-byte-array [l] (.getBytes (binding [*print-dup* false]
+                                  (pr-str l))
+                                "utf-8"))
+
+  clojure.lang.APersistentVector
+  (to-byte-array [v] (.getBytes (binding [*print-dup* false]
+                                  (pr-str v))
+                                "utf-8"))
+  clojure.lang.APersistentMap
+  (to-byte-array [m] (.getBytes (binding [*print-dup* false]
+                                  (pr-str m))
+                                "utf-8"))
+
+  clojure.lang.APersistentSet
+  (to-byte-array [s] (.getBytes (binding [*print-dup* false]
+                                  (pr-str s))
+                                "utf-8")))
+
+
+(defprotocol IFilter
+  "Implement a Bloom Filter"
+  (add [this elem])
+  (maybe-contains? [this elem]))
